6a6657c482ec34822d25acc957d3228db11d3382
Ubiratan Leandro Kotuzi <birakotuzi@gmail.com>
2021-08-21 19:05:35 -0300

    atualizaçãos no fomulário de cadastro

--- app/src/main/java/br/edu/ifsp/scl/sdm/cadastro/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/sdm/cadastro/MainActivity.java
@@ -5,14 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText nomeEt;
     private EditText telefoneEt;
     private EditText emailEt;
+    private CheckBox ingressar_listaCb;
+    private RadioButton masculinoRb;
     private EditText cidadeEt;
+    private Spinner estadoSp;
     private Button salvarBt;
 
     @Override
@@ -23,8 +30,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         nomeEt = findViewById(R.id.nomeEt);
         telefoneEt = findViewById(R.id.telefoneEt);
         emailEt = findViewById(R.id.emailEt);
+        ingressar_listaCb = findViewById(R.id.ingressar_listaCb);
+        masculinoRb = findViewById(R.id.masculinoRb);
         cidadeEt = findViewById(R.id.cidadeEt);
-
+        estadoSp = findViewById(R.id.estadoSp);
         salvarBt = findViewById(R.id.salvarBt);
         salvarBt.setOnClickListener(this);
     }
@@ -33,21 +42,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         StringBuilder sb = new StringBuilder();
         //nome
+        sb.append("Nome: ");
         sb.append(nomeEt.getText().toString());
         sb.append('\n');
 
         //telefone
+        sb.append("Telefone: ");
         sb.append(telefoneEt.getText().toString());
         sb.append('\n');
 
         //email
+        sb.append("Email: ");
         sb.append(emailEt.getText().toString());
         sb.append('\n');
 
+        //Ingressar lista de emails
+        sb.append("Ingressar na lista de emails? ");
+        sb.append(ingressar_listaCb.isChecked() ? "Sim" : "Não");
+        sb.append('\n');
+
+        //sexo
+        sb.append("Sexo: ");
+        sb.append(masculinoRb.isChecked() ? "Masculino" : "Feminino");
+        sb.append('\n');
+
         //cidade
+        sb.append("Cidade: ");
         sb.append(cidadeEt.getText().toString());
         sb.append('\n');
 
+        // Spinner
+        sb.append("Estado: ");
+        sb.append(((TextView) estadoSp.getSelectedView()).getText());
+        sb.append('\n');
 
         Toast.makeText(this, sb, Toast.LENGTH_SHORT).show();
     }
